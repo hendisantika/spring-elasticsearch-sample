58d62d4bf25e76744f993face38b2d080f128885
Hendi Santika <hendisantika@gmail.com>
2019-12-27 06:34:12 +0700

    #12 - Add EmployeeRepositoryTest

---
+++ src/test/java/com/hendisantika/springelasticsearchsample/repository/EmployeeRepositoryTest.java
@@ -0,0 +1,23 @@
+package com.hendisantika.springelasticsearchsample.repository;
+
+import org.junit.FixMethodOrder;
+import org.junit.runner.RunWith;
+import org.junit.runners.MethodSorters;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-elasticsearch-sample
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 27/12/19
+ * Time: 06.33
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+class EmployeeRepositoryTest {
+
+}
