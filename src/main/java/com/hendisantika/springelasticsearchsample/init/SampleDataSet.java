0ea001b102589d3d5565b4ab56fc527480c0202e
Hendi Santika <hendisantika@gmail.com>
2019-12-27 06:27:59 +0700

    #8 - Add SampleDataSet

---
+++ src/main/java/com/hendisantika/springelasticsearchsample/init/SampleDataSet.java
@@ -0,0 +1,20 @@
+package com.hendisantika.springelasticsearchsample.init;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-elasticsearch-sample
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 27/12/19
+ * Time: 06.27
+ */
+public class SampleDataSet {
+    private static final Logger LOGGER = LoggerFactory.getLogger(SampleDataSet.class);
+    private static final String INDEX_NAME = "sample";
+    private static final String INDEX_TYPE = "employee";
+    private static int COUNTER = 0;
+}
