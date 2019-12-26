0c99c5d669930435c7f366a203ed213f6085b51d
Hendi Santika <hendisantika@gmail.com>
2019-12-27 06:27:00 +0700

    #7 - Add EmployeeRepository

---
+++ src/main/java/com/hendisantika/springelasticsearchsample/repository/EmployeeRepository.java
@@ -0,0 +1,23 @@
+package com.hendisantika.springelasticsearchsample.repository;
+
+import com.hendisantika.springelasticsearchsample.domain.Employee;
+import org.springframework.data.elasticsearch.repository.ElasticsearchCrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-elasticsearch-sample
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 27/12/19
+ * Time: 06.26
+ */
+@Repository
+public interface EmployeeRepository extends ElasticsearchCrudRepository<Employee, Long> {
+    List<Employee> findByOrganizationName(String name);
+
+    List<Employee> findByName(String name);
+}
