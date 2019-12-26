f9093939378e8dd6d3a9d0794c92620675c33e54
Hendi Santika <hendisantika@gmail.com>
2019-12-27 06:18:37 +0700

    #1 - Initial commit

---
+++ src/main/java/com/hendisantika/springelasticsearchsample/SpringElasticsearchSampleApplication.java
@@ -0,0 +1,13 @@
+package com.hendisantika.springelasticsearchsample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringElasticsearchSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringElasticsearchSampleApplication.class, args);
+	}
+
+}
