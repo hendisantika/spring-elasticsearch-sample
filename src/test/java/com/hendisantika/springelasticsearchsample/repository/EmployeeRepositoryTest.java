f4ee5539ce6d0ee89ecba05cd8ef64770df3b048
Hendi Santika <hendisantika@gmail.com>
2019-12-27 06:35:32 +0700

    #14 - Add testAdd in EmployeeRepositoryTest

--- src/test/java/com/hendisantika/springelasticsearchsample/repository/EmployeeRepositoryTest.java
+++ src/test/java/com/hendisantika/springelasticsearchsample/repository/EmployeeRepositoryTest.java
@@ -1,8 +1,9 @@
 package com.hendisantika.springelasticsearchsample.repository;
 
-import org.junit.BeforeClass;
-import org.junit.ClassRule;
-import org.junit.FixMethodOrder;
+import com.hendisantika.springelasticsearchsample.domain.Department;
+import com.hendisantika.springelasticsearchsample.domain.Employee;
+import com.hendisantika.springelasticsearchsample.domain.Organization;
+import org.junit.*;
 import org.junit.runner.RunWith;
 import org.junit.runners.MethodSorters;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -31,5 +32,19 @@ class EmployeeRepositoryTest {
     @BeforeClass
     public static void before() {
         System.setProperty("spring.data.elasticsearch.cluster-nodes", container.getContainerIpAddress() + ":" + container.getMappedPort(9300));
+    }
+
+    @Test
+    public void testAdd() {
+        Employee employee = new Employee();
+//        employee.setId(1L);
+        employee.setName("John Smith");
+        employee.setAge(33);
+        employee.setPosition("Developer");
+        employee.setDepartment(new Department(1L, "TestD"));
+        employee.setOrganization(new Organization(1L, "TestO", "Test Street No. 1"));
+        employee = repository.save(employee);
+        Assert.assertNotNull(employee);
+        Assert.assertNotNull(employee.getId());
     }
 }
