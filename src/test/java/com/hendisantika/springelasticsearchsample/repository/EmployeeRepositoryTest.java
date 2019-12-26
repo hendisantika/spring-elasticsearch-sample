ef59f93af6e616b57699a2101613002295ed4896
Hendi Santika <hendisantika@gmail.com>
2019-12-27 06:34:34 +0700

    #13 - Add Before in EmployeeRepositoryTest

--- src/test/java/com/hendisantika/springelasticsearchsample/repository/EmployeeRepositoryTest.java
+++ src/test/java/com/hendisantika/springelasticsearchsample/repository/EmployeeRepositoryTest.java
@@ -1,10 +1,14 @@
 package com.hendisantika.springelasticsearchsample.repository;
 
+import org.junit.BeforeClass;
+import org.junit.ClassRule;
 import org.junit.FixMethodOrder;
 import org.junit.runner.RunWith;
 import org.junit.runners.MethodSorters;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.testcontainers.elasticsearch.ElasticsearchContainer;
 
 /**
  * Created by IntelliJ IDEA.
@@ -19,5 +23,13 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 @FixMethodOrder(MethodSorters.NAME_ASCENDING)
 class EmployeeRepositoryTest {
+    @ClassRule
+    public static ElasticsearchContainer container = new ElasticsearchContainer();
+    @Autowired
+    EmployeeRepository repository;
 
+    @BeforeClass
+    public static void before() {
+        System.setProperty("spring.data.elasticsearch.cluster-nodes", container.getContainerIpAddress() + ":" + container.getMappedPort(9300));
+    }
 }
